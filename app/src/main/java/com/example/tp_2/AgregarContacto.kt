0c00e62bafdbe8fdcaeaad43201f39c91f0b0842
pedrodom300211 <pedrodinguez2003@gmail.com>
2024-09-04 18:59:10 -0300

    Termine con parte del diseño

--- app/src/main/java/com/example/tp_2/AgregarContacto.kt
+++ app/src/main/java/com/example/tp_2/AgregarContacto.kt
@@ -1,9 +1,11 @@
 package com.example.tp_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.Toast
@@ -22,6 +24,16 @@ class AgregarContacto : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Evento click boton siguiente
+        val btnSiguiente :Button=findViewById(R.id.buttonContinuar)
+        btnSiguiente.setOnClickListener{
+            val intent = Intent(this, MasDatosUsuario::class.java)
+            startActivity(intent)
+        }
+
+
+
+
         val spinner: Spinner =findViewById(R.id.spinnerTelefono)
         val items = resources.getStringArray(R.array.listado_items_Telefono_Email)
         val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,items)
@@ -32,7 +44,25 @@ class AgregarContacto : AppCompatActivity() {
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, posicion: Int, ID: Long) {
                 val selectedItem=items[posicion]
-                Toast.makeText(this@AgregarContacto, "$selectedItem", Toast.LENGTH_SHORT).show()
+               // Toast.makeText(this@AgregarContacto, "$selectedItem", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+        val spinnerEmail: Spinner =findViewById(R.id.spinnerEmail)
+        val itemsEmail = resources.getStringArray(R.array.listado_items_Telefono_Email)
+        val adapterEmail = ArrayAdapter(this,android.R.layout.simple_spinner_item,items)
+        adapterEmail.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinnerEmail.adapter=adapterEmail
+
+        spinnerEmail.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, posicion: Int, ID: Long) {
+                val selectedItem=itemsEmail[posicion]
+                // Toast.makeText(this@AgregarContacto, "$selectedItem", Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
